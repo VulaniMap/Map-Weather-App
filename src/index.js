bd28270afdd68cdad9d432b6420c1b2257edf7d6
VulaniMap <vulanimaps@gmail.com>
2026-04-17 16:18:31 +0200

    Search-city

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+function searchCity(city) {
+  let apiKey = "aofcd5541add57c0396398488b47at43";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let city = document.querySelector("#current-city");
   city.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchSubmit);
