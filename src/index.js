9acac996b94d593d1b5bd9fe6535a86335705d87
VulaniMap <vulanimaps@gmail.com>
2026-04-17 15:48:01 +0200

    JavaScript form

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function searchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let city = document.querySelector("#current-city");
+  city.innerHTML = searchInput.value;
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", searchSubmit);
