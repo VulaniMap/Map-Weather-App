6ee566aa5d53619758b0498e909e1b36ac026932
VulaniMap <vulanimaps@gmail.com>
2026-04-20 15:20:18 +0200

    weather API

--- src/index.js
+++ src/index.js
@@ -2,9 +2,44 @@ function updateWeather(response) {
   let valueElement = document.querySelector("#value");
   let value = response.data.temperature.current;
   let city = document.querySelector("#current-city");
+  let description = document.querySelector("#description");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+  let time = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
+  time.innerHTML = formatDate(date);
+  wind.innerHTML = `${response.data.wind.speed}km/h`;
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  description.innerHTML = response.data.condition.description;
   city.innerHTML = response.data.city;
   valueElement.innerHTML = Math.round(value);
+}
+function formatDate(date) {
+  let day = date.getDay();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[date.getDay()];
+  return `${formattedDay} ${hours}:${minutes}`;
 }
 function searchCity(city) {
   let apiKey = "aofcd5541add57c0396398488b47at43";
