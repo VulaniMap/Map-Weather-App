b279b98cc71309f9fb29b9bdc8fbe4f37132fd02
VulaniMap <vulanimaps@gmail.com>
2026-04-20 13:07:21 +0200

    API update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
+function updateWeather(response) {
+  let valueElement = document.querySelector("#value");
+  let value = response.data.temperature.current;
+  let city = document.querySelector("#current-city");
+
+  city.innerHTML = response.data.city;
+  valueElement.innerHTML = Math.round(value);
+}
 function searchCity(city) {
   let apiKey = "aofcd5541add57c0396398488b47at43";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(refreshWeather);
+  axios.get(apiUrl).then(updateWeather);
 }
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let city = document.querySelector("#current-city");
-  city.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchSubmit);
+
+searchCity("Cape Town");
